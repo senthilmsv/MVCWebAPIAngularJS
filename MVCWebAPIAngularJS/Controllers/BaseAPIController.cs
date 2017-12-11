bb571abcbb4b9f7e8f9f194e86ae03fec27819bb
Senthil <senthil.msv@live.com>
2017-12-10 19:14:22 -1000

    Add project files.

---
+++ MVCWebAPIAngularJS/Controllers/BaseAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MVCWebAPIAngularJS.DBContext;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace MVCWebAPIAngularJS.Controllers
+{
+    public class BaseAPIController : ApiController
+    {
+        protected readonly UserDbEntities UserDB = new UserDbEntities();
+        protected HttpResponseMessage ToJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
---
+++ MVCWebAPIAngularJS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCWebAPIAngularJS.MvcApplication" Language="C#" %>
